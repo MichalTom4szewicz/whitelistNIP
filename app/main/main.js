5fcd0a5e7452612ac991e663a07dd0b3a2b5519b
unknown <235568@student.pwr.edu.pl>
2020-09-29 20:53:32 +0200

    History file encryption works, sometimes throws errors
    can do 1 proper save per applicantion run, then need a restart

--- app/main/main.js
+++ app/main/main.js
@@ -3,60 +3,23 @@ const fs = require('fs');
 const crypto = require('crypto')
 const readline = require('readline');
 
-const processLineByLine = async (path) => {
-  const fileStream = fs.createReadStream(path);
-
-  const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
-  });
-
-  let linesArray = []
-  for await (const line of rl) {
-    // console.log(line)
-    // linesArray.push(line)
-
-    const decryptedLine = decrypt(line)
-    linesArray.push(decryptedLine)
-    console.log(`Line from file: ${decryptedLine}`);
-  }
-
-  return linesArray
-}
 
 const enKey = crypto.createCipher('aes-128-cbc', 'dupa');
 const deKey = crypto.createDecipher('aes-128-cbc', 'dupa');
 
-const encrypt = (data) => {
-  let mystr = enKey.update(data, 'utf8', 'hex')
-  mystr += enKey.final('hex');
+const encrypt = (text) => {
+  let encrypted = enKey.update(text, 'utf8', 'hex')
+  encrypted += enKey.final('hex');
 
-  return mystr
+  return encrypted
 }
 
-const decrypt = (data) => {
-  let str = deKey.update(data, 'hex', 'utf8')
-  str += deKey.final('utf8');
+const decrypt = (encrypted) => {
+  let text = deKey.update(encrypted, 'hex', 'utf8')
+  text += deKey.final('utf8');
 
-  return str
+  return text
 }
-
-
-const appendToFile = (path, data) => {
-  // console.log('start appending')
-  let lines = Array.from(processLineByLine(path))
-  lines.push(data)
-  console.log('tablica', lines)
-
-  const encryptedData = encrypt(Buffer.from(lines))
-
-  fs.writeFile(path, Buffer.from(lines), function (err) {
-    if (err) return console.log(err);
-  });
-
-  // console.log(lines)
-}
-
 
 ipcMain.on('data', (event, arg) => {
   // console.log('mamaaaaaaaa data', arg)
@@ -82,8 +45,93 @@ ipcMain.on('data', (event, arg) => {
   let data = arg
   data.date = day+'-'+month+'-'+year+' '+hour+':'+minutes;
 
-  const finalString = data.nip+','+data.two+','+data.four+','+data.account+','+data.result+','+data.date+'\n'
+  const finalString = data.nip+';'+data.two+';'+data.four+';'+data.account+';'+data.result+';'+data.date
 
-  appendToFile(`${month}-${year}_WHResults.txt`, finalString )
+  fs.readFile(`${month}-${year}_WHResults.txt`, 'utf8' , (err, data) => {
 
+    // console.log('enc',data, typeof(data))
+
+    let dec = decrypt(data.replace(/[^\x20-\x7E]/g, ''))
+
+    // console.log(dec)
+
+    let d = Array.from(dec.split(','))
+
+    d.push(finalString.toString())
+    console.log(d)
+    const enc = encrypt(d.toString())
+
+    // console.log('enc', enc , typeof(enc))
+
+    fs.writeFile(`${month}-${year}_WHResults.txt`, enc, function (err) {
+      if (err) return console.log(err);
+      console.log('saved')
+    });
+
+
+  })
+
+  // fs.readFile(`${month}-${year}_WHResults.txt`, 'utf8' , (err, data) => {
+
+  //   if(err) {
+  //     fs.writeFile(`${month}-${year}_WHResults.txt`, '', function (err) {
+  //       if (err) return console.log(err);
+  //       console.log('created')
+
+  //       let d = []
+
+  //       d.push(finalString)
+  //       console.log(d)
+
+  //       const enc = encrypt(d.toString())
+
+  //       fs.writeFile(`${month}-${year}_WHResults.txt`, enc, function (err) {
+  //         if (err) return console.log(err);
+  //         console.log('saved')
+  //       });
+  //     });
+  //   } else {
+  //     let dec = decrypt(data)
+
+  //     // console.log(dec)
+
+  //     let d = Array.from(dec.split(','))
+
+  //     d.push(finalString)
+  //     console.log(d)
+  //     console.log('str', d.toString())
+
+  //     const enc = encrypt(d.toString())
+
+  //     // console.log('enc', enc , typeof(enc))
+
+  //     fs.writeFile(`${month}-${year}_WHResults.txt`, enc, function (err) {
+  //       if (err) return console.log(err);
+  //       console.log('saved')
+  //     });
+  //   }
+  // })
 })
+
+
+// fs.readFile('enDupa.txt', 'utf8' , (err, data) => {
+
+//   console.log('enc',data, typeof(data))
+
+//   let dec = decrypt(data)
+
+//   console.log(dec)
+
+//   let d = Array.from(dec.split(','))
+
+//   d.push('kurde;bele')
+
+//   const enc = encrypt(d.toString())
+
+//   console.log('enc', enc , typeof(enc))
+
+//   fs.writeFile('enDupa.txt', enc, function (err) {
+//     if (err) return console.log(err);
+//     console.log('saved')
+//   });
+
